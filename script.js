cbd82fc1e72ac6a97cb80a9840400ee9519da8d1
olamide <olamideashaolu318.gmail.com>
2025-06-17 15:57:35 +0100

    commit new codes

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 let speech = new SpeechSynthesisUtterance();
 let voices = [];
 let voiceSelect = document.querySelector('select');
+let button = document.querySelector('button');
+let textarea = document.querySelector('textarea');
 
-window.speechSynthesis.onvoiceschanged = () => {
+function populateVoices() {
     voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0];
-
-    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
-   
-    voiceSelect.addEventListener('change', () => {
-        speech.voice = voices[voiceSelect.value];
+    voiceSelect.innerHTML = ''; // Clear previous options
+    voices.forEach((voice, i) => {
+        let option = new Option(voice.name, i);
+        voiceSelect.add(option);
     });
-
+    // Set default voice
+    if (voices.length > 0) {
+        speech.voice = voices[0];
+        voiceSelect.value = 0;
+    }
 }
 
-document.querySelector('button').addEventListener('click', () =>{
-    speech.text = document.querySelector('textarea').value;
+populateVoices();
+window.speechSynthesis.onvoiceschanged = populateVoices;
+
+voiceSelect.addEventListener('change', () => {
+    speech.voice = voices[voiceSelect.value];
+});
+
+button.addEventListener('click', () => {
+    speech.text = textarea.value;
     window.speechSynthesis.speak(speech);
 });
